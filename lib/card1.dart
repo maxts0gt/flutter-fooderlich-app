16b3e16301ad4affae020eccdc17514db8a0d490
Max <bazarsadtsogtjargal@gmail.com>
2021-11-29 21:18:28 +0900

    add text and finish card1

--- lib/card1.dart
+++ lib/card1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
 
 class Card1 extends StatelessWidget {
   const Card1({Key? key}) : super(key: key);
@@ -14,23 +15,47 @@ class Card1 extends StatelessWidget {
     return Center(
       // TODO: Card1 Decorate Container
       child: Container(
-        // TODO: Add a stack of text
-        // 1
+        child: Stack(
+          children: [
+            Text(
+              category,
+              style: FooderlichTheme.darkTextTheme.bodyText1,
+            ),
+            Positioned(
+              child: Text(
+                title,
+                style: FooderlichTheme.darkTextTheme.headline2,
+              ),
+              top: 20,
+            ),
+            Positioned(
+              child: Text(
+                description,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 30,
+              right: 0,
+            ),
+            Positioned(
+              child: Text(
+                chef,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 10,
+              right: 0,
+            )
+          ],
+        ),
         padding: const EdgeInsets.all(16),
-        // 2
         constraints: const BoxConstraints.expand(
           width: 350,
           height: 450,
         ),
-        // 3
         decoration: const BoxDecoration(
-          // 4
           image: DecorationImage(
-            // 5
             image: AssetImage('assets/mag1.png'),
-            // 6
             fit: BoxFit.cover,
-          ), // 7
+          ),
           borderRadius: BorderRadius.all(Radius.circular(10.0)),
         ),
       ),
