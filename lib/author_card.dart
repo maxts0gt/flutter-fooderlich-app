d484a7240b3ded460365d94293c40a805e5fed10
Max <bazarsadtsogtjargal@gmail.com>
2021-12-02 15:11:54 +0900

    changed author_card to stateful widget and activated favorite button

--- lib/author_card.dart
+++ lib/author_card.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'fooderlich_theme.dart';
 import 'circle_image.dart';
 
-class AuthorCard extends StatelessWidget {
+class AuthorCard extends StatefulWidget {
   // 1
   final String authorName;
   final String title;
@@ -13,7 +13,13 @@ class AuthorCard extends StatelessWidget {
     required this.title,
     this.imageProvider,
   }) : super(key: key);
-  // 2
+
+  @override
+  State<AuthorCard> createState() => _AuthorCardState();
+}
+
+class _AuthorCardState extends State<AuthorCard> {
+  bool _isFavorited = false;
   @override
   Widget build(BuildContext context) {
     // TODO: Replace return Container(...);
@@ -26,7 +32,7 @@ class AuthorCard extends StatelessWidget {
           Row(
             children: [
               CircleImage(
-                imageProvider: imageProvider,
+                imageProvider: widget.imageProvider,
                 imageRadius: 28,
               ),
               // 2
@@ -36,11 +42,11 @@ class AuthorCard extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    authorName,
+                    widget.authorName,
                     style: FooderlichTheme.lightTextTheme.headline2,
                   ),
                   Text(
-                    title,
+                    widget.title,
                     style: FooderlichTheme.lightTextTheme.headline3,
                   ),
                 ],
@@ -48,15 +54,19 @@ class AuthorCard extends StatelessWidget {
             ],
           ),
           IconButton(
-              // 4
-              icon: const Icon(Icons.favorite_border),
-              iconSize: 30,
-              color: Colors.grey[400],
-              // 5
-              onPressed: () {
-                const snackBar = SnackBar(content: Text('Favorite Pressed'));
-                ScaffoldMessenger.of(context).showSnackBar(snackBar);
-              }),
+            // 4
+            icon: Icon(_isFavorited ? Icons.favorite : Icons.favorite_border),
+            iconSize: 30,
+            color: Colors.red[400],
+            // 5
+            onPressed: () {
+              setState(
+                () {
+                  _isFavorited = !_isFavorited;
+                },
+              );
+            },
+          ),
         ],
       ),
     );
