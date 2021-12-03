01b28803ba8ae186d82acb80c589d071e118a4c6
Max <bazarsadtsogtjargal@gmail.com>
2021-12-03 13:06:26 +0900

    adding nested list view

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -6,31 +6,29 @@ import '../api/mock_fooderlich_service.dart';
 class ExploreScreen extends StatelessWidget {
   // 1
   final mockService = MockFooderlichService();
+
   ExploreScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-// 1
     return FutureBuilder(
-      // 2
       future: mockService.getExploreData(),
-      // 3
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
-        // TODO: Add Nested List Views
-        // 4
         if (snapshot.connectionState == ConnectionState.done) {
-          // 5
-          final recipes = snapshot.data?.todayRecipes ?? [];
-          return TodayRecipeListView(recipes: recipes);
-          return Center(
-            child: Container(
-              child: const Text('Show TodayRecipeListView'),
-            ),
+          return ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
+              const SizedBox(height: 16),
+              // TODO: Replace this with FriendPostListView
+              Container(
+                height: 400,
+                color: Colors.green,
+              ),
+            ],
           );
         } else {
-          // 6
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
+          return const Center(child: CircularProgressIndicator());
         }
       },
     );
